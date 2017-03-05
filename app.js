28c0b854673a5a9d68b5bd801d1d01c0449ac430
megmatty <meghopweb@me.com>
2017-03-04 22:10:48 -0800

    start screen, initial answer wiring

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+// $(document).ready(function(){};
+
+
+
 var url = "https://opentdb.com/api.php?amount=10&type=multiple"; //gets mc questions
 
-	var response; //holds response data
-	var counter = 0; //counter for getting next question
+var response; //holds response data
+var counter = 0; //counter for getting next question
+var uniqueRandoms = []; //empty array for random numbers
+var numRandoms = 4; //how many numbers do you want (1-4)
+var correct = 0;
+var incorrect = 0;
 
-	$.getJSON(url, getQuestions);
+
+//AJAX call
+
+$.getJSON(url, getQuestions);
+
 
 
 function getQuestions(data) {
@@ -27,41 +39,64 @@ function buildQuestion(num) {
 		//randomize answer button text
 			//new function to take 1,2,3,4 and randomize
 				//math.random + loop to place answers in ids
-				//then assign 'mc-' + makeUniqueRandom 
+				//then assign 'mc-' + makeUniqueRandom
 }		
 
 //Get Next Button
-$('#next').click(function(event) { //next button
+function nextQuestion() { //next button
 	if (counter <=8) {
 		counter = (counter + 1); //advance counter by 1
 	} else if (counter >=9) {
 		counter = 0; //reset counter
-		$('#results').toggleClass('hide'); //hide question area
-		$('#winner').toggleClass('hide'); //display win text
+		$('#game').toggleClass('hide'); //hide question area
 	}
 
+	$('.answer-buttons').removeClass('green-button');
+	$('.answer-buttons').removeClass('red-button');
 	buildQuestion(counter);
 
-}); //in final build, this should automatically advance without button
+}; //in final build, this should automatically advance without button
+
+//setTimeout(function(){...},2000)
+
 
 
 //Answer button handler
 $('.answer-buttons').click(function(event) {
 	var correctAnswer = response.results[counter].correct_answer; //gets correct answer value from object
-	$('#correct-holder').html(correctAnswer); //converts unicode to html in hidden div
-	var realAnswer = $('#correct-holder').text(); //gets the text inside the hidden div
+
+	var realAnswer = $('<p>' + correctAnswer + '</p>').text(); //gets the text inside the hidden div
+	
 	if ( $(this).text() == realAnswer ) { //compares text of button with text of realAnswer
-		$('#correct').text('You are right!');
+		$(this).addClass('green-button').stop().delay(1800).queue(function() { //light green and wait
+			nextQuestion(); //get next question
+		});
+		correct++;
 	} else {
-		$('#correct').html(' No, the answer is ' + response.results[counter].correct_answer);
+		$(this).addClass('red-button').stop().delay(1200).queue(function() { //delay
+			$('.answer-buttons').filter(function() {
+					return $(this).text() == realAnswer;
+				}).addClass('green-button').stop().delay(2000).queue(function() {
+					nextQuestion(); //move to next question
+				});
+			});
+
+
+
+		incorrect++;
 	}
-	//probably needs its own function to check answers, how to get "this"??
+	// displayScore(); //score
+
 });
+
+// function displayScore() { //displays score
+// 	score = correct / (correct + incorrect);
+// 	console.log(score);
+// 	console.log(correct * 10);
+// }
 
 
 //Random number generator for multiple choice answers - thanks stackoverflow!
-	var uniqueRandoms = []; //empty array
-	var numRandoms = 4; //how many numbers do you want (1-4)
 
 	function makeUniqueRandom() {
 	    // refill the array if needed
@@ -79,23 +114,132 @@ $('.answer-buttons').click(function(event) {
 	    return val;
 	}
 
-// $('button').click(function(event) {
-// 	$('#example').load('example.html');
-// });
+//Category Selector
+$('.category').on('click', function(event) {
+	var text = $(this).text();
+	for (var i = 0; i < categories.length; i++) {
+		if (text === categories[i].name) {
+			url = url + "&category=" + categories[i].num;
+		}
+	}
+	console.log(url);
 
-$('#category-1').on('click', function(event) {
-	url = url + "&category=9";
 	$.getJSON(url, getQuestions);
+	counter = 0;
+	correct = 0;
+	incorrect = 0;
+	buildQuestion(counter);
+	$(this).addClass('green-button');
 });
 
 
+//Hide start screen & load game screen
+$(".start-button").click(function(event) {
+	$('.tagline').hide();
+	$('.instructions').hide();
+	$("#category").hide();
+	$(this).hide();
+	$("#game").fadeIn('slow').css('display', 'flex');
+});
 
-//make simple object to match category name to the category id from OTDB
-//click handler for "this" button that pulls from that object by matching the text
+// $(window).ready(function(){//when window is ready
+// 	var user = window.localStorage.getItem("user");
+// 	if (!user) {
+// 		console.log("no user");
+// 		user = "meg";
+// 		window.localStorage.setItem("user", user);
+// 	}
+
+// 	console.log(user);
+// }); 
+
+// var random = Math.floor(Math.random()*10);
+// $(".category").eq(random).click();
 
 
+var categories = [ //API does not supply categories numbers in JSON response
+	{
+		name: "General Knowledge",
+	 	num : 9
+	},
+	{
+		name: "Books",
+		num: 10
+	},	
+	{
+		name: "Film",
+		num: 11
+	},
+	{
+		name: "Music",
+		num: 12
+	},
+	{
+		name: "Television",
+		num: 14
+	},
+	{	
+		name: "Science & Nature",
+		num: 17
+	},
+	{
+		name: "Geography",
+		num: 22
+	},
+	{
+		name: "Art",
+		num: 25
+	},
+	{
+		name: "Sports",
+		num: 21
+	},
+	{
+		name: "Animals",
+		num: 27
+	},
 
-
+	{
+		name: "Mythology",
+		num: 20
+	},
+	{
+		name: "Comics",
+		num: 29
+	},
+	{
+		name: "History",
+		num: 23
+	},
+	{
+		name: "Politics",
+		num: 24
+	},
+	{
+		name: "Computers",
+		num: 18
+	},
+	{
+		name: "Celebrities",
+		num: 26
+	},
+	{
+		name: "Board Games",
+		num: 16
+	},
+	{
+		name: "Mathematics",
+		num: 19
+	},
+	{
+		name: "Vehicles",
+		num: 28
+	},
+	{
+		name: "Cartoons",
+		num: 32
+	}
+];
 
 
 
